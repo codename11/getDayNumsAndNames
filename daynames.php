14b31ad7cfdec7831e3f059d9cf2b10ae31df80f
Veljko <veljkos82@gmail.com>
2021-04-18 00:34:47 +0200

    Finds names for days in particular month of particular year.

---
+++ daynames.php
@@ -0,0 +1,26 @@
+<?php 
+
+    function getDayNumsAndNames($calendar, $date){
+
+        $obj = new stdClass();
+        $str = strtotime($date);
+        $obj->monthNum = date("m", $str);
+        $obj->month = date("F", $str);
+        $obj->year = date("Y", $str);
+
+        $numOfDays=cal_days_in_month(CAL_GREGORIAN,$obj->monthNum,$obj->year);
+
+        $obj->dayNamesInMonth = [];
+        for ($i = 1; $i <= $numOfDays; $i++) {
+            $obj->dayNamesInMonth[$i] = date("l", strtotime($i."-".$obj->monthNum."-".$obj->year));
+        }
+
+        echo(json_encode($obj));
+
+    }
+    
+    if(isset($_POST) && !empty($_POST["datum"])){
+        getDayNumsAndNames(1, $_POST["datum"]);
+    }
+    
+?>
